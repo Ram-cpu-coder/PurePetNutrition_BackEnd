6d70b8dcefe7941d173c0b36cd2a2445bd4d20fc
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-09-04 23:54:18 +1000

    fix: cors.php

---
+++ config/cors.php
@@ -0,0 +1,15 @@
+<?php
+$allowedOrigins = ['http://127.0.0.1:5500', 'https://frontend.com'];
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, X-CSRF-Token");
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}

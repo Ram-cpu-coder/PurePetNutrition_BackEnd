022f7dd77f1a28a54eee360d331de61c297c4f24
bishal khatri <bishal.khatri343@outlook.com>
2025-09-05 23:08:49 +0545

    minor fixes

--- config/db.php
+++ config/db.php
@@ -4,10 +4,10 @@ require_once __DIR__ . '/config.php';
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 // Hardcode CORS for local testing
-header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, X-CSRF-Token");
+// header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
+// header("Access-Control-Allow-Credentials: true");
+// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// header("Access-Control-Allow-Headers: Content-Type, X-CSRF-Token");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo 'hello world';
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
